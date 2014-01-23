55f6466c0ef300574dcdb2fd7e5dab441c657bdd
Luke Griffiths <wlgriffiths@gmail.com>
2014-01-23 15:31:18 -0500

    use latest ActiveSupport interface even before Railties

--- lib/authorize_net.rb
+++ lib/authorize_net.rb
@@ -84,9 +84,16 @@ if defined?(Rails)
   else
     %w{ models controllers helpers }.each do |dir|
       path = File.join(File.dirname(__FILE__), 'app', dir)
+
       $LOAD_PATH << path
-      ActiveSupport::Dependencies.load_paths << path
-      ActiveSupport::Dependencies.load_once_paths.delete(path)
+
+      if ActiveSupport::Dependencies.respond_to?(:autoload_paths)
+        ActiveSupport::Dependencies.autoload_paths << path
+        ActiveSupport::Dependencies.autoload_once_paths.delete(path)
+      else
+        ActiveSupport::Dependencies.load_paths << path
+        ActiveSupport::Dependencies.load_once_paths.delete(path)
+      end
     end
   end
 end
